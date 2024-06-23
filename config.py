5beb98968060532dcc485ebbb425b35050f4c342
STTech1 <123819448+STTech1@users.noreply.github.com>
2024-06-23 18:43:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,27 +7,27 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID = os.environ.get("API_ID", "")  # ⚠️ Required
-    API_HASH = os.environ.get("API_HASH", "")  # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")  # ⚠️ Required
+    API_ID = os.environ.get("API_ID", "17777279")  # ⚠️ Required
+    API_HASH = os.environ.get("API_HASH", "f2cb943ac7421015761dd8f0615a31d4")  # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5402380823:AAHOkeFHcWTyPvt8NGItK8ScoxlfX3gfJq8")  # ⚠️ Required
 
     # premium 4g renaming client
-    STRING_API_ID = os.environ.get("STRING_API_ID", "29759992")
-    STRING_API_HASH = os.environ.get("STRING_API_HASH", "61f150cdca64b2916fa499d107393140")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "BQHGGfgAYnI0zaGWwYVGHINHADJ6LzeTW1wbLLs9MToDm_GjKBHZGd2sSCtiroCzSvM6QilyMfjurX-MOLrm0WQMVC0EdPUX4W6pvUrVEoqnU37lQLouMCTmcJApnfkdg_W_0lXwN1jGuvGXpM07Peb1t3Nu-H0fEmaPiiGtfCtbsfX-s9eufbuKJBOdeAWF1SfMrvkAiL33tyVOq8jwVTkH-1HLZuY7yhp63B4uNPtgh_ie8xXBzpHNcf7-Cd3ni43ujXSgRsfop_3LTfyqqU9dDD0EYv59DPlfP-tlgQDye06V1uBaKhnCzfx3G7Qs4JSPmWE00ZFpC3YfUL5V4mYIQBGUygAAAAFY9ke4AA")
+    STRING_API_ID = os.environ.get("STRING_API_ID", "26579601")
+    STRING_API_HASH = os.environ.get("STRING_API_HASH", "5b811e9e89adfe6c636d3328b247779d")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "BAGVkpEAFYW5aOLygAbeu_7hjo-DiJoyxvULO4lsyfSfu7Yz5rkDSLXpiRgXsZz24nRELyObhC5_BTkmqZex7jatTV7lNOjsVfvC9g7DwUPH5nHqnuADByqz9H_vamPauda70Y80QnBktW1ty_c8iOkKad9tQcPnldsXFGOcFaw1D9zQnhZYfDjOYpGqhAtkNmF3mrLLtTSx0K_tril_5e1Eoi7OVjEdY-hqrWg6N1jQyA3MljEHQDQW48Et0SHL7bNYh_COOMlGtChAeNAcnC6NkFPfVJXTtvGZUskBPmyuWbWwiq2O_9UkC-cJ28a5ycfjir8CzFqr7CiBUt8JRBjiULR7tAAAAAGXnJoVAA")
 
     # database config
-    DB_NAME = os.environ.get("DB_NAME", "Snow_User_Data")
-    DB_URL = os.environ.get("DB_URL", "")  # ⚠️ Required
+    DB_NAME = os.environ.get("DB_NAME", "second2")
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://second2:second2@cluster0.pbi0w9t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # ⚠️ Required
 
     # other configs
     BOT_UPTIME = time.time()
-    START_PIC = os.environ.get("START_PIC", "")
+    START_PIC = os.environ.get("START_PIC", "https://drive.google.com/file/d/1RoD-Fg37-I0v4sjMRCZR-s5Bf16CIkTf/view?usp=drivesdk")
     ADMIN = [int(admin) if id_pattern.search(
-        admin) else admin for admin in os.environ.get('ADMIN', '').split()]  # ⚠️ Required
+        admin) else admin for admin in os.environ.get('ADMIN', '5076254266').split()]  # ⚠️ Required
     
-    FORCE_SUB = os.environ.get("FORCE_SUB", "") # ⚠️ Required Username without @
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))  # ⚠️ Required
+    FORCE_SUB = os.environ.get("FORCE_SUB", "st_update") # ⚠️ Required Username without @
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001806676469"))  # ⚠️ Required
     FLOOD = int(os.environ.get("FLOOD", '10'))
     BANNED_USERS = set(int(x) for x in os.environ.get(
         "BANNED_USERS", "1234567890").split())
